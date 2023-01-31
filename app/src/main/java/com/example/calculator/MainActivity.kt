2ea9347916a6e53574f929740e8c0de45ddcb659
finog <finogenov.07@mail.ru>
2023-01-31 17:39:53 +0300

    First Commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,182 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+                ) {
+
+                    mainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun mainScreen() {
+
+    var a = 0
+    var alisa by rememberSaveable() {
+        mutableStateOf("")
+    }
+    var result by rememberSaveable() {
+        mutableStateOf(0)
+    }
+    var garrry by rememberSaveable() {
+        mutableStateOf(0)
+    }
+
+
+
+
+    Column {
+
+        Text(text = "$alisa, $garrry", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        Text(text = "$result", fontWeight = FontWeight.ExtraBold, fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Row() {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "%")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "CE")
+            }
+            Button(onClick = {
+                alisa = ""
+                result = 0
+                a = 0
+            }) {
+                Text(text = "C")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Del")
+            }
+        }
+        Row() {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "1/x")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "x^2")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "sqrt")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "/")
+            }
+        }
+        Row() {
+            Button(onClick = { alisa += "7" }) {
+                Text(text = "7")
+            }
+            Button(onClick = { alisa += "8" }) {
+                Text(text = "8")
+            }
+            Button(onClick = { alisa += "9" }) {
+                Text(text = "9")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "*")
+            }
+        }
+        Row() {
+            Button(onClick = { alisa += "4" }) {
+                Text(text = "4")
+            }
+            Button(onClick = { alisa += "5" }) {
+                Text(text = "5")
+            }
+            Button(onClick = { alisa += "6" }) {
+                Text(text = "6")
+            }
+            Button(onClick = {
+                if (a == 1) {
+                    result = 0
+                    alisa = ""
+                    a = 0
+                } else {
+                    garrry = alisa.toInt()
+                    result -= garrry
+                    alisa = ""
+                }
+            }) {
+                Text(text = "-")
+            }
+        }
+        Row() {
+            Button(onClick = { alisa += "1" }) {
+                Text(text = "1")
+            }
+            Button(onClick = { alisa += "2" }) {
+                Text(text = "2")
+            }
+            Button(onClick = { alisa += "3" }) {
+                Text(text = "3")
+            }
+            Button(onClick = {
+
+                if (a == 1) {
+                    result = 0
+                    alisa = ""
+                    a = 0
+                } else {
+                    garrry = alisa.toInt()
+                    result += garrry
+                    alisa = ""
+                }
+                Log.d("$garrry - G | $result - R | $alisa - A", "results")
+            }) {
+                Text(text = "+")
+            }
+
+        }
+        Row() {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "+/-")
+            }
+            Button(onClick = { alisa += "0" }) {
+                Text(text = "0")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = ".")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "=")
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
